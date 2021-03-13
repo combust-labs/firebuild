02ad81798e29474120eae1897ed83ec9ee1bfa4b
Radek Gruchalski <radek@gruchalski.com>
2021-03-13 14:53:49 +0000

    Trace ls command

--- cmd/ls/cmd.go
+++ cmd/ls/cmd.go
@@ -3,12 +3,15 @@ package ls
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/combust-labs/firebuild/configs"
+	"github.com/combust-labs/firebuild/pkg/tracing"
 	"github.com/combust-labs/firebuild/pkg/utils"
 	"github.com/combust-labs/firebuild/pkg/vmm"
+	"github.com/opentracing/opentracing-go"
 	"github.com/spf13/cobra"
 )
 
@@ -21,13 +24,15 @@ var Command = &cobra.Command{
 }
 
 var (
-	logConfig = configs.NewLogginConfig()
-	runCache  = configs.NewRunCacheConfig()
+	logConfig     = configs.NewLogginConfig()
+	runCache      = configs.NewRunCacheConfig()
+	tracingConfig = configs.NewTracingConfig("vmm-ls")
 )
 
 func initFlags() {
 	Command.Flags().AddFlagSet(logConfig.FlagSet())
 	Command.Flags().AddFlagSet(runCache.FlagSet())
+	Command.Flags().AddFlagSet(tracingConfig.FlagSet())
 }
 
 func init() {
@@ -35,35 +40,85 @@ func init() {
 }
 
 func run(cobraCommand *cobra.Command, _ []string) {
+	os.Exit(processCommand())
+}
+
+func processCommand() int {
+
 	cleanup := utils.NewDefers()
 	defer cleanup.CallAll()
+
 	rootLogger := logConfig.NewLogger("ls")
+
+	tracer, tracerCleanupFunc, tracerErr := tracing.GetTracer(rootLogger.Named("tracer"), tracingConfig)
+	if tracerErr != nil {
+		rootLogger.Error("failed constructing tracer", "reason", tracerErr)
+		return 1
+	}
+
+	cleanup.Add(tracerCleanupFunc)
+
+	spanLs := tracer.StartSpan("ls")
+	defer spanLs.Finish()
+
+	itemsWithMetadata := 0
+	itemsWithoutMetadata := 0
+
 	fileInfos, readDirErr := ioutil.ReadDir(runCache.RunCache)
 	if readDirErr != nil {
 		rootLogger.Error("error listing run cache directory", "reason", readDirErr)
 	}
 	for _, fileInfo := range fileInfos {
+
 		// see if the pid file can be loaded:
 		vmmID := fileInfo.Name()
+
+		spanVMM := tracer.StartSpan("vmm-fetch-metadata", opentracing.ChildOf(spanLs.Context()))
+		spanVMM.SetTag("vmm-id", vmmID)
+
 		vmmMetadata, hasMetadata, err := vmm.FetchMetadataIfExists(filepath.Join(runCache.RunCache, vmmID))
 		if err != nil {
 			rootLogger.Error("failed loading metadata file for possible VMM", "vmm-id", vmmID, "reason", err)
+			spanVMM.SetBaggageItem("error", err.Error())
+			spanVMM.Finish()
 			continue
 		}
+
+		spanVMM.SetTag("has-metadata", hasMetadata)
+		spanVMM.Finish()
+
 		if hasMetadata {
+
+			spanVMMPID := tracer.StartSpan("vmm-pid-check", opentracing.ChildOf(spanVMM.Context()))
+
+			itemsWithMetadata = itemsWithMetadata + 1
 			running, err := vmmMetadata.PID.IsRunning()
 			if err != nil {
 				rootLogger.Error("failed checking pid status for possible VMM", "vmm-id", vmmID, "reason", err)
+				spanVMMPID.SetBaggageItem("error", err.Error())
+				spanVMMPID.Finish()
 				continue
 			}
+
 			rootLogger.Info("vmm", "id", vmmID,
 				"running", running,
 				"pid", vmmMetadata.PID.Pid,
 				"image", fmt.Sprintf("%s/%s:%s", vmmMetadata.Rootfs.Image.Org, vmmMetadata.Rootfs.Image.Image, vmmMetadata.Rootfs.Image.Version),
 				"started", time.Unix(vmmMetadata.StartedAtUTC, 0).UTC().String(),
 				"ip-address", vmmMetadata.NetworkInterfaces[0].StaticConfiguration.IPConfiguration.IP)
+
+			spanVMMPID.SetTag("is-running", running)
+			spanVMMPID.Finish()
+
 		} else {
+			itemsWithoutMetadata = itemsWithoutMetadata + 1
 			rootLogger.Info("vmm", "id", vmmID, "running", "???", "pid", "???")
 		}
+
 	}
+
+	spanLs.SetBaggageItem("with-metadata", fmt.Sprintf("%d", itemsWithMetadata))
+	spanLs.SetBaggageItem("without-metadata", fmt.Sprintf("%d", itemsWithoutMetadata))
+
+	return 0
 }
