e647ff197c0083d3eda55c17700509f42a9ad6fb
Radek Gruchalski <radek@gruchalski.com>
2021-03-15 00:59:48 +0000

    Change the VMM ID length to 20 characters

--- pkg/utils/randstring.go
+++ pkg/utils/randstring.go
@@ -5,12 +5,22 @@ import (
 )
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const letterDigitBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 // RandStringBytes returns a random string of length n.
 func RandStringBytes(n int) string {
 	b := make([]byte, n)
 	for i := range b {
 		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+// RandStringWithDigitsBytes returns a random string of length n.
+func RandStringWithDigitsBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterDigitBytes[rand.Intn(len(letterDigitBytes))]
 	}
 	return string(b)
 }
