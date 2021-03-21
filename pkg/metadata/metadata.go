d2a0e36597162f1cbc986c8656f5f9c749eaff51
Radek Gruchalski <radek@gruchalski.com>
2021-03-21 14:50:05 +0000

    Correct the MMDS payload, accepts strings only. Payload structure.

--- pkg/metadata/metadata.go
+++ pkg/metadata/metadata.go
@@ -1,6 +1,9 @@
 package metadata
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/combust-labs/firebuild/configs"
 	"github.com/combust-labs/firebuild/pkg/utils"
 	"github.com/combust-labs/firebuild/pkg/vmm/pid"
@@ -53,7 +56,7 @@ type MDNetStaticConfiguration struct {
 	HostDevName string `json:"host-dev-name" mapstructure:"host-dev-name"`
 	// IPConfiguration (optional) allows a static IP, gateway and up to 2 DNS nameservers
 	// to be automatically configured within the VM upon startup.
-	IPConfiguration *MDNetIPConfiguration `json:"ip-configuration" mapstructure:"ip=configuration"`
+	IPConfiguration *MDNetIPConfiguration `json:"ip-configuration" mapstructure:"ip-configuration"`
 }
 
 // MDNetworkInterafce is network interface configuration of a running VMM.
@@ -138,19 +141,65 @@ func (r *MDRun) AsMMDS() (interface{}, error) {
 	metadata := &MMDSLatest{
 		Latest: &MMDSLatestMetadata{
 			Metadata: &MMDSData{
-				VMMID:             r.VMMID,
-				Drives:            r.Drives,
-				Env:               env,
-				Hostname:          r.Configs.RunConfig.Hostname,
-				MachineConfig:     r.Configs.Machine,
-				NetworkInterfaces: r.NetworkInterfaces,
-				Rootfs:            r.Rootfs,
-				SSHKeys: func() []string {
-					resp := []string{}
-					for _, key := range keys {
-						resp = append(resp, string(utils.MarshalSSHPublicKey(key)))
+				VMMID: r.VMMID,
+				Drives: func() map[string]*MMDSDrive {
+					result := map[string]*MMDSDrive{}
+					for _, drive := range r.Drives {
+						result[*drive.DriveID] = &MMDSDrive{
+							DriveID:      *drive.DriveID,
+							IsReadOnly:   fmt.Sprintf("%v", *drive.IsReadOnly),
+							IsRootDevice: fmt.Sprintf("%v", *drive.IsRootDevice),
+							Partuuid:     drive.Partuuid,
+							PathOnHost:   *drive.PathOnHost,
+						}
 					}
-					return resp
+					return result
+				}(),
+				Env:           env,
+				LocalHostname: r.Configs.RunConfig.Hostname,
+				Machine: &MMDSMachine{
+					CPU:         fmt.Sprintf("%d", r.Configs.Machine.CPU),
+					CPUTemplate: r.Configs.Machine.CPUTemplate,
+					HTEnabled:   fmt.Sprintf("%v", r.Configs.Machine.HTEnabled),
+					KernelArgs:  r.Configs.Machine.KernelArgs,
+					Mem:         fmt.Sprintf("%d", r.Configs.Machine.Mem),
+					VMLinuxID:   r.Configs.Machine.VMLinuxID,
+				},
+				Network: &MMDSNetwork{
+					CNINetworkName: r.Configs.Machine.CNINetworkName,
+					Interfaces: func() map[string]*MMDSNetworkInterface {
+						result := map[string]*MMDSNetworkInterface{}
+						for _, nic := range r.NetworkInterfaces {
+							result[nic.StaticConfiguration.MacAddress] = &MMDSNetworkInterface{
+								HostDevName: nic.StaticConfiguration.HostDevName,
+								Gateway:     nic.StaticConfiguration.IPConfiguration.Gateway,
+								IfName:      nic.StaticConfiguration.IPConfiguration.IfName,
+								IP:          nic.StaticConfiguration.IPConfiguration.IP,
+								IPAddr:      nic.StaticConfiguration.IPConfiguration.IPAddr,
+								IPMask:      nic.StaticConfiguration.IPConfiguration.IPMask,
+								IPNet:       nic.StaticConfiguration.IPConfiguration.IPNet,
+								Nameservers: strings.Join(nic.StaticConfiguration.IPConfiguration.Nameservers, ","),
+							}
+						}
+						return result
+					}(),
+					SSHPort: fmt.Sprintf("%d", r.Configs.Machine.SSHPort),
+				},
+				ImageTag: r.Rootfs.Tag,
+				Users: func() map[string]*MMDSUser {
+					result := map[string]*MMDSUser{}
+					if r.Configs.Machine.SSHUser != "" {
+						result[r.Configs.Machine.SSHUser] = &MMDSUser{
+							SSHKeys: func() string {
+								resp := []string{}
+								for _, key := range keys {
+									resp = append(resp, string(utils.MarshalSSHPublicKey(key)))
+								}
+								return strings.Join(resp, "\n")
+							}(),
+						}
+					}
+					return result
 				}(),
 			},
 		},
--- pkg/metadata/mmds.go
+++ pkg/metadata/mmds.go
@@ -1,8 +1,6 @@
 package metadata
 
 import (
-	"github.com/combust-labs/firebuild/configs"
-	"github.com/firecracker-microvm/firecracker-go-sdk/client/models"
 	"github.com/mitchellh/mapstructure"
 )
 
@@ -23,12 +21,50 @@ type MMDSLatestMetadata struct {
 }
 
 type MMDSData struct {
-	VMMID             string                 `json:"vmm-id" mapstructure:"vmm-id"`
-	Drives            []models.Drive         `json:"drivers" mapstructure:"drives"`
-	Env               map[string]string      `json:"env" mapstructure:"env"`
-	Hostname          string                 `json:"hostname" mapstructure:"hostname"`
-	MachineConfig     *configs.MachineConfig `json:"machine" mapstructure:"machine"`
-	NetworkInterfaces []MDNetworkInterafce   `json:"network-interfaces" mapstructure:"network-interfaces"`
-	Rootfs            *MDRootfs              `json:"rootfs" mapstructure:"rootfs"`
-	SSHKeys           []string               `json:"ssh-keys" mapstructure:"ssh-keys"`
+	VMMID         string                `json:"vmm-id" mapstructure:"vmm-id"`
+	Drives        map[string]*MMDSDrive `json:"drives" mapstructure:"drives"`
+	Env           map[string]string     `json:"env" mapstructure:"env"`
+	LocalHostname string                `json:"local-hostname" mapstructure:"local-hostname"`
+	Machine       *MMDSMachine          `json:"machine" mapstructure:"machine"`
+	Network       *MMDSNetwork          `json:"network" mapstructure:"network"`
+	ImageTag      string                `json:"image-tag" mapstructure:"image-tag"`
+	Users         map[string]*MMDSUser  `json:"users" mapstructure:"users"`
+}
+
+type MMDSDrive struct {
+	DriveID      string `json:"drive-id" mapstructure:"drive-id"`
+	IsReadOnly   string `json:"is-read-only" mapstructure:"is-read-only"`
+	IsRootDevice string `json:"is-root-device" mapstructure:"is-root-device"`
+	Partuuid     string `json:"partuuid" mapstructure:"partuuid"`
+	PathOnHost   string `json:"path-on-host" mapstructure:"path-on-host"`
+}
+
+type MMDSNetwork struct {
+	CNINetworkName string                           `json:"cni-network-name" mapstructure:"cni-network-name"`
+	Interfaces     map[string]*MMDSNetworkInterface `json:"interfaces" mapstructure:"interfaces"`
+	SSHPort        string                           `json:"ssh-port" mapstructure:"ssh-port"`
+}
+
+type MMDSNetworkInterface struct {
+	HostDevName string `json:"host-dev-name" mapstructure:"host-dev-name"`
+	Gateway     string `json:"gateway" mapstructure:"gateway"`
+	IfName      string `json:"ifname" mapstructure:"ifname"`
+	IP          string `json:"ip" mapstructure:"ip"`
+	IPAddr      string `json:"ip-addr" mapstructure:"ip-addr"`
+	IPMask      string `json:"ip-mask" mapstructure:"ip-mask"`
+	IPNet       string `json:"ip-net" mapstructure:"ip-net"`
+	Nameservers string `json:"nameservers" mapstructure:"nameservers"`
+}
+
+type MMDSUser struct {
+	SSHKeys string `json:"ssh-keys" mapstructure:"ssh-keys"`
+}
+
+type MMDSMachine struct {
+	CPU         string `json:"cpu" mapstructure:"cpu"`
+	CPUTemplate string `json:"cpu-template" mapstructure:"cpu-template"`
+	HTEnabled   string `json:"ht-enabled" mapstructure:"ht-enabled"`
+	KernelArgs  string `json:"kernel-args" mapstructure:"kernel-args"`
+	Mem         string `json:"mem" mapstructure:"mem"`
+	VMLinuxID   string `json:"vmlinux" mapstructure:"vmlinux"`
 }
